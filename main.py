c4c4e81268f5f0da594556d67125aecccfc1d28e
CMMProfeDuoc <ca.felipe@profesor.duoc.cl>
2024-06-11 15:59:53 -0400

    tareas por hacer

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import libreria_pokemon as libPKM
 
+"""
+    AGREGAR:
+    - loadPkm
+    - menu para hacer cosas con el pkm
+    - iniciar programa (?)
+    - crear pokemon segun entrada de usuario
+    - agregar atributos a pkm (?)
+    - verificar si la especia de un pokemon existe <- tendran que de alguna forma escribir las 1000+ especies (o buscar la lista lista en algun lado)
+"""
+
+
 pepito = libPKM.loadPkm('')
 
 print(pepito)
