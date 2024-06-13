f15aa47bba24be63e5c1ff18a4726e67abfdd23f
CAFSMM <cafs.munozmontoya@gmail.com>
2024-06-13 16:19:52 -0400

    update

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import acciones_pokemon as accion
     - menu para hacer cosas con el pkm
     - iniciar programa (?)
     - crear pokemon segun entrada de usuario
-    - agregar atributos a pkm (?)
-    - verificar si la especia de un pokemon existe <- tendran que de alguna forma escribir las 1000+ especies (o buscar la lista lista en algun lado)
+    ✔ verificar si la especia de un pokemon existe <- tendran que de alguna forma escribir las 1000+ especies (o buscar la lista lista en algun lado)
 """
 
 
